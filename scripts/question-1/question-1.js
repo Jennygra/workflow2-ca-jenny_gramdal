fc08e8ab413b2e801e40a7aa23205162b87daf2a
Jennygra <83270722+Jennygra@users.noreply.github.com>
2022-11-16 13:45:26 +0100

    Questions for the CA

---
+++ scripts/question-1/question-1.js
@@ -0,0 +1,21 @@
+// Create User interface. The types are:
+//  school: string
+//  firstName: string
+//  lastName: string
+//  age: number
+//  isAdmin: OPTIONAL boolean
+
+// Convert the function to TypeScript, making sure to use the
+//  User interface as the return type
+function createUser(firstName, lastName, age, isAdmin = false) {
+  return {
+    school: 'Noroff',
+    firstName,
+    lastName,
+    age,
+    isAdmin,
+  };
+}
+
+const newUser = createUser('Ola', 'Nordmann', 18);
+const newAdmin = createUser('Kari', 'Nordmann', 36, true);
